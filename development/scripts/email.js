7e17a692cdc55eb85ce4ae5bb382f1a79e8698c3
mgarbacz <mich.garbacz@gmail.com>
2014-10-07 20:31:46 -0500

    Cleanup and normalize javascript

--- development/scripts/email.js
+++ development/scripts/email.js
@@ -1,44 +1,40 @@
 $(document).ready(function() {
 
-  $("#address")
-      .focus(function () { $(this).select(); } )
-      .mouseup(function (e) {e.preventDefault(); });
+    $("#address")
+        .focus(function() { $(this).select(); } )
+        .mouseup(function(e) {e.preventDefault(); });
 
 
-  function getVar(variable)
-  {
-         var query = window.location.search.substring(1);
-         var vars = query.split("&");
-         for (var i=0;i<vars.length;i++) {
-                 var pair = vars[i].split("=");
-                 if(pair[0] == variable){return pair[1];}
-         }
-         return(false);
-  }
+    function getVar(variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
 
-  if(getVar("email") == "valid"){
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if (pair[0] == variable)
+                return pair[1];
+        }
 
-    $("#address").notify("Thank you for subscribing! We will keep you up to date!", 
-      {
+        return(false);
+    }
 
-      autoHide: false,
-      elementPosition: 'bottom',
-      className: 'success',
+    if (getVar("email") == "valid") {
 
-      });
+        $("#address").notify("Thank you for subscribing! We will keep you up to date!", {
+            autoHide: false,
+            elementPosition: 'bottom',
+            className: 'success',
+        });
 
-  }else if(getVar("email") == "invalid"){
+    } else if (getVar("email") == "invalid") {
 
-    $("#address").notify("Please enter a valid email address.", 
-      {
+        $("#address").notify("Please enter a valid email address.", {
+          autoHide: false,
+          elementPosition: 'bottom',
+          className: 'warn',
+        });
 
-      autoHide: false,
-      elementPosition: 'bottom',
-      className: 'warn',
-
-      });
-      
-  }
+    }
 
 });
 
--- development/scripts/scrollHint.js
+++ development/scripts/scrollHint.js
@@ -1,53 +1,43 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	if($(document).scrollTop() == 0){
+    if ($(document).scrollTop() == 0) {
 
-		$("#scrollHint").fadeIn();
+        $("#scrollHint").fadeIn();
 
-	}
-	$(window).scroll( function() {
+    }
+    $(window).scroll(function() {
 
-		if($(document).scrollTop() < 1 ) {
+        if ($(document).scrollTop() < 1) {
+            $("#scrollHint img").fadeIn();
+        } else {
+            $("#scrollHint img").fadeOut();
+        }
 
-			$("#scrollHint img").fadeIn();
-	
-		} else {
+    });
+    $('#scrollHint img').hover(function() {
+        $(this).fadeTo(500, .8);
+    }, function() {
+        $(this).fadeTo(500, .5);
+    });
 
-			$("#scrollHint img").fadeOut();
-	
-		}
-	});
-	$('#scrollHint img').hover(function() {
-	
-		$(this).fadeTo(500, .8);
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
 
-	}, function(){
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 
-		$(this).fadeTo(500, .5);
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 
-	});
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
 
-	$(function() {
+        });
 
-	  $('a[href*=#]:not([href=#])').click(function() {
+    });
 
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-
-			var target = $(this.hash);
-
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-
-		if (target.length) {
-
-	        $('html,body').animate({
-
-				scrollTop: target.offset().top
-
-			}, 1000);
-			return false;
-
-	      }
-	    }
-	  });
-	});
 });
