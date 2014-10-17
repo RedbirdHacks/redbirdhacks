9586f39da7edf82201258a0dea304256d81c40c0
mgarbacz <mich.garbacz@gmail.com>
2014-10-16 23:21:41 -0500

    Bring master root up to date with development

--- scripts/scrollHint.js
+++ scripts/scrollHint.js
@@ -1,53 +1,54 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	if($(document).scrollTop() == 0){
+    if ($(document).scrollTop() == 0) {
 
-		$("#scrollHint").fadeIn();
+        $("#scrollHint").fadeIn();
 
-	}
-	$(window).scroll( function() {
+    }
+    $(window).scroll(function() {
 
-		if($(document).scrollTop() < 1 ) {
+        if ($(document).scrollTop() < 1) {
+            $("#scrollHint img").fadeIn();
+        } else {
+            $("#scrollHint img").fadeOut();
+        }
 
-			$("#scrollHint img").fadeIn();
-	
-		} else {
 
-			$("#scrollHint img").fadeOut();
-	
-		}
-	});
-	$('#scrollHint img').hover(function() {
-	
-		$(this).fadeTo(500, .8);
+        // TODO maybe not in this file!
+        if ($(document).scrollTop() > $('#hero').height()) {
+            $('#main-nav').addClass('affix');
+            $('#faq').css('margin-top', $('#main-nav').height());
+        }
+        else {
+            $('#main-nav').removeClass('affix');
+            $('#faq').css('margin-top', '0');
+        }
 
-	}, function(){
+    });
+    $('#scrollHint img').hover(function() {
+        $(this).fadeTo(500, .8);
+    }, function() {
+        $(this).fadeTo(500, .5);
+    });
 
-		$(this).fadeTo(500, .5);
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
 
-	});
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 
-	$(function() {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 
-	  $('a[href*=#]:not([href=#])').click(function() {
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                }
+            }
 
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        });
 
-			var target = $(this.hash);
+    });
 
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-
-		if (target.length) {
-
-	        $('html,body').animate({
-
-				scrollTop: target.offset().top
-
-			}, 1000);
-			return false;
-
-	      }
-	    }
-	  });
-	});
 });
